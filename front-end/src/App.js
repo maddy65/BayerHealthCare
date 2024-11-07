0969b5d97bf855f0e1830746eaff3292eaf41825
Ramesh E <rameshe@Rameshs-MacBook-Pro.local>
2024-11-07 17:22:07 +0530

    provide page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
+import ProviderProfile from "./pages/ProviderProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<ProviderProfile />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
