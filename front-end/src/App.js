e2a18bcc33d35208f7fffa0af5e3840028dc25fb
Abhinag Ujire Mohan <36569424+Abhinagum@users.noreply.github.com>
2024-11-07 17:54:20 +0530

    Landing page Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
 import ProviderProfile from "./pages/ProviderProfile";
 import BookAnAppoinment from "./pages/BookAnAppoinment";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<ProviderProfile />} />
-          <Route path="/book-an-appointment" element={<BookAnAppoinment />} />
+        <Route path="/profile" element={<ProviderProfile />} />
+        <Route path="/book-an-appointment" element={<BookAnAppoinment />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
---
+++ front-end/src/pages/Landing.js
@@ -0,0 +1,112 @@
+import React from "react";
+import "./main.css";
+
+export default function Landing() {
+  return (
+    <>
+      <div className="header-part">
+        <h1 className="text-bar">Bayer Healthcare</h1>
+        <div className="menu-bar">
+          <ul className="menu-bar">
+            <li>
+              <a href="##">Home</a>
+            </li>
+            <li>
+              <a href="##">Health Topics</a>
+            </li>
+            <li>
+              <a href="##">Resources</a>
+            </li>
+            <li>
+              <a href="##">About US</a>
+            </li>
+            <li>
+              <a href="##">Contact</a>
+            </li>
+            <li>
+              <a href="##">Log In</a>
+            </li>
+          </ul>
+        </div>
+        <h1 className="text-bar-one">Your Health,Our Priority</h1>
+        <h5 className="text-bar-two">
+          Explore the latest health information and resources from Bayer
+          Healthcare
+        </h5>
+      </div>
+      <div className="banner-container">
+        <h3 className="text-F">Featured Health Topics</h3>
+        <div className="banner-bar">
+          <div className="cont-one">
+            <h2 className="header-text">COVID-19 Updates</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header">
+              <a href="##">Learn More</a>
+            </button>
+          </div>
+          <div className="cont-one">
+            <h2 className="header-text">COVID-19 Updates</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header">
+              <a href="##">Learn More</a>
+            </button>
+          </div>
+          <div className="cont-one">
+            <h2 className="header-text">COVID-19 Updates</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header">
+              <a href="##">Learn More</a>
+            </button>
+          </div>
+          <div className="cont-one">
+            <h2 className="header-text">COVID-19 Updates</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header">
+              <a href="##">Learn More</a>
+            </button>
+          </div>
+        </div>
+      </div>
+
+<div className="banner-container-one">
+
+        <div className="banner-bar-one">
+        <h3 className="text-F">Featured Health Topics</h3>
+          <div className="cont-one-one">
+            <h2 className="header-text-one">Bayer Launches New Initiative for Preventive Healthcare</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header-one">
+              <a href="##">Read Full Article</a>
+            </button>
+          </div>
+       
+          <div className="cont-one-one-two">
+            <h2 className="header-text-one">Bayer Launches New Initiative for Preventive Healthcare</h2>
+            <p>
+              Stay informed about the latest COVID-19 guidelines and vaccination
+              information.
+            </p>
+            <button className="btn-header-one">
+            <a href="##">Read Full Article</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
