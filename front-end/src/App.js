f92b02810d7d3ab81cb798239ae51b472df06ca2
Abhinag Ujire Mohan <36569424+Abhinagum@users.noreply.github.com>
2024-11-07 17:10:01 +0530

    Book An Appointment Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
+import BookAnAppoinment from "./pages/BookAnAppoinment";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/book-an-appointment" element={<BookAnAppoinment />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
