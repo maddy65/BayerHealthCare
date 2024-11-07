a2475561f541ab6f08535102022251f5318d445d
Ramesh E <rameshe@Rameshs-MacBook-Pro.local>
2024-11-07 16:12:00 +0530

    log in page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
